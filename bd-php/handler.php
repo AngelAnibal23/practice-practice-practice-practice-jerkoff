40fa7df2f85ad70041354b0ed3f0f72a938627e1
Guts <aamamanica@unjbg.edu.pe>
2026-01-13 18:11:07 -0500

    adding instructions to handler's form

---
+++ bd-php/handler.php
@@ -0,0 +1,31 @@
+<?php
+
+include 'connectionDB.php'; 
+
+$dotaherosID = mysqli_real_escape_string($conexion, $_POST['dotaheros']);
+$matches = mysqli_real_escape_string($conexion, $_POST['matches']); 
+$winrate = mysqli_real_escape_string($conexion, $_POST['winrate']);
+$kda = mysqli_real_escape_string($conexion, $_POST['kda']);
+$role = mysqli_real_escape_string($conexion, $_POST['role']);
+$lane = mysqli_real_escape_string($conexion, $_POST['lane']);
+
+$sql = "UPDATE heroesimg 
+        SET matches = '$matches', 
+            winrate = '$winrate', 
+            KDA = '$kda', 
+            rol = '$role', 
+            lane = '$lane' 
+        WHERE id = '$dotaherosID'";
+$response = mysqli_query($conexion, $sql); 
+
+if ($response){
+    echo "Succes"; 
+    echo "<a href='../html/index.html'>← Volver</a>";
+}
+else{
+    echo "Wrong"; 
+}
+
+mysqli_close($conexion);
+
+?>
